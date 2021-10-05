01dc32911f56ca1e32cd0ceff1cbb350190592f9
Nacheez <87002244+Nacheez@users.noreply.github.com>
2021-10-05 21:16:38 +0300

    Update hw-1.sql

--- hw-1.sql
+++ hw-1.sql
@@ -5,20 +5,20 @@ CREATE TABLE subscriptions(
 );
 
 INSERT INTO "user" (id,first_name, last_name, username, password, birth_date, gender, user_pic)
-VALUES (11, 'Bob', 'Li', 'Bob_Li', 'qwerty', '2000-02-02', 'TRUE', null),
-		(22, 'Jo', 'Ji', 'Jo_Ji','123', '2001-03-03', 'TRUE', null);
+VALUES (1, 'Bob', 'Li', 'Bob_Li', 'qwerty', '2000-02-02', 'TRUE', null),
+		(2, 'Jo', 'Ji', 'Jo_Ji','123', '2001-03-03', 'TRUE', null);
 
 INSERT INTO subscriptions (follower_id, author_id)
-VALUES (11, 22);
+VALUES (1, 2);
 
 INSERT INTO tweet (id, user_id, text, photo, pub_date, replies_to, retweets)
-VALUES (3, 22, 'Have a good day!', '', '2021-10-05', null, null);
+VALUES (1, 2, 'Have a good day!', '', '2021-10-05', null, null);
 
 INSERT INTO hashtag (id, text)
-VALUES (4, 'tag');
+VALUES (1, 'tag');
 
 INSERT INTO tweethashtag (id, tweet_id, hashtag_id)
-VALUES (5, 3, 4);
+VALUES (1, 1, 1);
 
 INSERT INTO user_like(id, user_id, tweet_id)
-VALUES (7, 11, 3);
+VALUES (1, 1, 1);
